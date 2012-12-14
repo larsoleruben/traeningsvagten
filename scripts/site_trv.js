6987de7079c9b5b83c9683483a13da08ca6baea0
larsoleruben <l.christoffersen@gmail.com>
2012-12-14 21:59:49 +0100

    First commit

---
+++ scripts/site_trv.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: lars
+ * Date: 12/14/12
+ * Time: 7:19 PM
+ * To change this template use File | Settings | File Templates.
+ */
+$(document).ready(function()    {
+    //add the datepicker to the consumed day
+    $("#datepicker").datepicker();
+
+});
