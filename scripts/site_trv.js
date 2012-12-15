8feec20f5863d3d447134a14a45f50ba3b04214f
larsoleruben <l.christoffersen@gmail.com>
2012-12-15 13:28:40 +0100

    Changes to layout and resizing problems

--- scripts/site_trv.js
+++ scripts/site_trv.js
@@ -5,8 +5,8 @@
  * Time: 7:19 PM
  * To change this template use File | Settings | File Templates.
  */
+
 $(document).ready(function()    {
     //add the datepicker to the consumed day
     $("#datepicker").datepicker();
-
 });
