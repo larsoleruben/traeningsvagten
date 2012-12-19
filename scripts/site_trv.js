f6efb852094b7eb0af649a75e2cf067ae78a5bda
larsoleruben <l.christoffersen@gmail.com>
2012-12-19 01:17:24 +0100

    Added popup for adding food

--- scripts/site_trv.js
+++ scripts/site_trv.js
@@ -6,57 +6,90 @@
  * To change this template use File | Settings | File Templates.
  */
 
-$(document).ready(function()    {
+$(document).ready(function () {
     //add the datepicker to the consumed day
+    $(function () {
+        $("#dialog-form").dialog({
+            autoOpen:false,
+            height:300,
+            width:350,
+            modal:true,
+            buttons:{
+                "Tilføj:":function () {
+
+                    $(this).dialog("close");
+
+                },
+                Cancel:function () {
+                    $(this).dialog("close");
+                }
+            }
+        });
+        $("#create-user").button()
+            .click(function () {
+                $("#dialog-form").dialog("open");
+            });
+    });
+
     var dag = new Date();
-    $('#datepicker').val(dag.getDate()+'/'+dag.getMonth()+'/'+dag.getFullYear());
+    $('#datepicker').val(dag.getDate() + '/' + dag.getMonth() + '/' + dag.getFullYear());
     $("#datepicker").datepicker();
 
     //get the food search
-    $('#search').change(function(){
-        queryFood( $('#search').val() );
-    });
-
-    $('#searchBtn').click(function(){
+    $('#search').change(function () {
         queryFood($('#search').val());
     });
 
-    function queryFood( searchString ){
+    $('#searchBtn').button().click(function () {
+        queryFood($('#search').val());
+    });
+
+    $('#saveDayButton').button();
+
+    $("#result").dblclick(function () {
+        //alert($("#result option:selected").val() + " " + $("#result option:selected").text());
+        $("#dialog-form").dialog("open");
+    });
+
+
+    function queryFood(searchString) {
         var url = "http://traeningsvagten.dk/Services/FoodInfoWebService.asmx/GetFoodInfoJson?nameLike=" + searchString +
-            "&random="+(new Date()).getTime();
+            "&random=" + (new Date()).getTime();
         var newScriptElement = document.createElement("script");
-        newScriptElement.setAttribute( "src", url );
-        newScriptElement.setAttribute( "id", "jsonp" );
-        var oldScriptElement = document.getElementById( "jsonp" );
-        var head = document.getElementsByTagName( "head")[0] ;
-        if( oldScriptElement == null ) {
-            head.appendChild( newScriptElement );
-        }else{
-            head.replaceChild( newScriptElement, oldScriptElement );
+        newScriptElement.setAttribute("src", url);
+        newScriptElement.setAttribute("id", "jsonp");
+        var oldScriptElement = document.getElementById("jsonp");
+        var head = document.getElementsByTagName("head")[0];
+        if (oldScriptElement == null) {
+            head.appendChild(newScriptElement);
+        } else {
+            head.replaceChild(newScriptElement, oldScriptElement);
         }
 
     }
+
 
 });
 
 
 /*is automatically called by the return values from queryFood   */
-function updateFood(foodValues){
+function updateFood(foodValues) {
     //clear up
     $('#result')
         .find('option')
         .remove()
         .end();
+    //add the new query result
     for (var i = 0; i < foodValues.length; i++) {
         var singleObject = foodValues[i];
         $('#result')
             .append($("<option></option>")
-            .attr("value",singleObject.FoodId)
+            .attr("value", singleObject.FoodId)
             .text(singleObject.DanName));
     }
 }
 
 /*
-* test code for finding food items, with associated values
-* */
+ * test code for finding food items, with associated values
+ * */
 
